f566636d49c3694e50a498a001102ee91ae50fed
Nathan <naslee2@gmail.com>
2020-02-09 21:37:54 -0800

    revised update to components

---
+++ client/src/Components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Aux from '../../Aux/Aux'
+import Navbar from '../Navbar/Navbar'
+import classes from './Layout.css'
+
+
+const layout = (props) => (
+    <Aux>
+        <Navbar/>
+        <main>
+        {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
---
+++ client/src/Components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Layout.css'
+
+const navbar = (props) => (
+    <header>
+        <div>
+            <p>test</p>
+        </div>
+    </header>
+);
+
+export default navbar
